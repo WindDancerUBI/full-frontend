ced9c83925e638342c0635201e125c00de4ca364
huangjitao <164173925@qq.com>
2023-04-15 20:32:39 +0800

    style：样式修改

--- src/components/BrowserWindow/index.tsx
+++ src/components/BrowserWindow/index.tsx
@@ -1,7 +1,7 @@
-import React, {type CSSProperties, type ReactNode} from 'react';
-import clsx from 'clsx';
+import React, { type CSSProperties, type ReactNode } from "react";
+import clsx from "clsx";
 
-import styles from './styles.module.css';
+import styles from "./styles.module.css";
 
 interface Props {
   children: ReactNode;
@@ -14,19 +14,25 @@ interface Props {
 export default function BrowserWindow({
   children,
   minHeight,
-  url = 'http://localhost:3000',
+  url = "http://localhost:3000",
   style,
   bodyStyle,
 }: Props): JSX.Element {
+  const openWindow = () => {
+    window.open(url, "_blank");
+  };
   return (
-    <div className={styles.browserWindow} style={{...style, minHeight}}>
+    <div className={styles.browserWindow} style={{ ...style, minHeight }}>
       <div className={styles.browserWindowHeader}>
         <div className={styles.buttons}>
-          <span className={styles.dot} style={{background: '#f25f58'}} />
-          <span className={styles.dot} style={{background: '#fbbe3c'}} />
-          <span className={styles.dot} style={{background: '#58cb42'}} />
+          <span className={styles.dot} style={{ background: "#f25f58" }} />
+          <span className={styles.dot} style={{ background: "#fbbe3c" }} />
+          <span className={styles.dot} style={{ background: "#58cb42" }} />
         </div>
-        <div className={clsx(styles.browserWindowAddressBar, 'text--truncate')}>
+        <div
+          className={clsx(styles.browserWindowAddressBar, "text--truncate")}
+          onClick={openWindow}
+        >
           {url}
         </div>
         <div className={styles.browserWindowMenuIcon}>
@@ -46,20 +52,25 @@ export default function BrowserWindow({
 }
 
 // Quick and dirty component, to improve later if needed
-export function IframeWindow({url}: {url: string}): JSX.Element {
-  let urlStr
+export function IframeWindow({ url }: { url: string }): JSX.Element {
+  let urlStr;
   if (url.includes("http") || url.includes("https")) {
-    urlStr = url
+    urlStr = url;
   } else {
-    urlStr = "https://winddancer.gitee.io" + url
+    urlStr = "https://winddancer.gitee.io" + url;
   }
   return (
     <div>
       <BrowserWindow
         url={urlStr}
         // style={{minWidth: '40vw', maxWidth: "100%"}}
-        bodyStyle={{padding: 0}}>
-        <iframe src={urlStr} title={urlStr} style={{width: '100%', height: 500}} />
+        bodyStyle={{ padding: 0 }}
+      >
+        <iframe
+          src={urlStr}
+          title={urlStr}
+          style={{ width: "100%", height: 500 }}
+        />
       </BrowserWindow>
     </div>
   );
